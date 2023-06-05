093053de7e6d2270d53f7ab143fb0a188bafae15
fhoang_ <87514379+hoangphan2504@users.noreply.github.com>
2023-06-06 00:05:59 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
   
-
+let paraInput = ""
 
 const app = express();
 
@@ -26,50 +26,82 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.send("API checking ")
+    res.send("API checking")
 })
 
 app.get("/api/check", async (req, res) => {
-    const input = req.query.input;
-    console.log("api checkk");
-    let Result = "";
-    console.log("check check");
-    if(input) {
-        const response = await openai.createChatCompletion({
+  const input = req.query.input;
+  paraInput = req.query.input;
+  console.log("api checkk");
+  let Result = "";
+  console.log("check check");
+
+  if (input) {
+    apiCallsMade = true;
+    try {
+      const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are a rude assistant. You only give the answer, what they ask and what user wants you to response like user ask you to return 1 only the response only contain number 1, you won't give any friendly response or give any additional feedback.",
+          },
+          {
+            role: "user",
+            content: `Please check the grammar of the following sentence while retaining the LaTeX syntax: ${input}. Then provide the corrected only, without any additional feedback and additional latex code? If there are no error, return 1 only, no more`,
+          },
+        ],
+      });
+
+      correctedGrammar = response.data.choices[0].message.content;
+      Result =  correctedGrammar;
+
+        res.status(200).json({output: Result});
+      
+    } catch (error) {
+      console.error("Error making API calls:", error);
+      res.status(500).json({ error: "An error occurred while processing the request" });
+    }
+  } else {
+    console.log("chua truy cap duoc if tren");
+    res.status(400).json({ error: "Invalid input" });
+  }
+});
+
+
+app.get("/api/para", async (req, res) => {
+  const input = paraInput;
+  console.log(input, "para");
+  let Result = "";
+  console.log("check check");
+  if(input != "") {
+        // paraphrase
+
+        const continuedResponse = await openai.createChatCompletion({
           model: "gpt-3.5-turbo",
           messages: [
-              {
-                role: "user",
-                content: `Please check the grammar of the following sentence while retaining the LaTeX syntax: ${input}. Then provide the corrected only, without any additional feedback and additional latex code?`
-              },
-              
-            ],
-          });
-          
-          const correctedGrammar = response.data.choices[0].message.content;
+          // Include previous conversation messages
+            { role: "user", 
+            content: `Provide a paraphrased version and retain the latex syntax (if it has), without any additional feedback :${input}` },
+          ]
+        });
 
-          // paraphrase
+        const paraphrase = continuedResponse.data.choices[0].message.content;
+        console.log("paraphrase",paraphrase);
 
-          const continuedResponse = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [
-            // Include previous conversation messages
-              { role: "user", 
-              content: `Provide a paraphrased version and retain the latex syntax (if it has), without any additional feedback :${correctedGrammar}` },
-            ]
-          });
+        paraResult =  paraphrase;
+        // Result = paraphrase;
 
-          const paraphrase = continuedResponse.data.choices[0].message.content;
-          console.log("paraphrase",paraphrase);
+        res.status(200).json({output: paraResult});
 
-          Result = {correctedGrammar, paraphrase};
-          // Result = paraphrase;
-
-          res.status(200).json({output: Result});
-
-    }
-    else console.log("chua truy cap duoc if tren");
+  }
+  else console.log("chua truy cap duoc if tren");
 })
+
+
+  
+
 
 app.listen(3001, function (err) {
     if (err) console.log(err);
